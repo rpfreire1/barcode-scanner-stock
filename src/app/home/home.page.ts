38f62941ed82b4c960784562409831c2bec914e0
rpfreire1 <rpfreire1@espe.edu.ec>
2023-12-15 15:10:17 -0500

    added methods and services

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 import { AlertController } from '@ionic/angular';
+import {ProductService} from "../core/services/product.service";
+import {Product} from "../core/types/product.type";
+import {FormBuilder, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -8,17 +11,41 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+
   isSupported = false;
   barcodes: Barcode[] = [];
   scannedBarcodeValue: string = '';
+  productSelected!:Product;
 
+  productForm = this.formBuilder.group({
+    productName: [{value: '', disabled: true}],
+    productCode: [{value: '', disabled: true}],
+    productStock: [{value: 0, disabled: true}],
+    productNewStock: [0]
+  });
 
-  constructor(private alertController: AlertController) {}
+  constructor(private formBuilder: FormBuilder,
+              private alertController: AlertController,
+              private productService:ProductService) {
+
+  }
 
   ngOnInit() {
     BarcodeScanner.isSupported().then((result) => {
       this.isSupported = result.supported;
     });
+    this.productForm=this.formBuilder.group(
+      {
+        productName: new FormControl(''),
+        productCode: new FormControl(''),
+        productStock: new FormControl(0),
+        productNewStock: new FormControl(0)
+
+      }
+    );
+
+
+
   }
 
   async scan(): Promise<void> {
@@ -30,11 +57,17 @@ export class HomePage implements OnInit {
     const { barcodes } = await BarcodeScanner.scan();
     this.barcodes.push(...barcodes);
     this.scannedBarcodeValue = barcodes.length > 0 ? barcodes[0].rawValue : '';
+    if(this.scannedBarcodeValue!=''){
+      this.getProductByBarCode(this.scannedBarcodeValue);
+    }
   }
 
   async requestPermissions(): Promise<boolean> {
     const { camera } = await BarcodeScanner.requestPermissions();
     return camera === 'granted' || camera === 'limited';
+  }
+  onFilterBoxChanged(){
+    this.getProductByBarCode(this.scannedBarcodeValue);
   }
 
   async presentAlert(): Promise<void> {
@@ -45,4 +78,39 @@ export class HomePage implements OnInit {
     });
     await alert.present();
   }
+  getProductByBarCode(barcode:string){
+    this.productService.getProductByBarCode(barcode).subscribe(product=>{
+      if (product){
+        this.productSelected=product;
+        this.productForm.patchValue(
+          {
+            productName: this.productSelected.nameProduct,
+            productCode: this.productSelected.barCodeProduct,
+            productStock: this.productSelected.stockProduct,
+            productNewStock: 0
+          }
+        )
+        this.productForm.get('productName')?.disable();
+        this.productForm.get('productCode')?.disable();
+        this.productForm.get('productStock')?.disable();
+
+
+      }
+
+    })
+  }
+
+  async saveNewStock(){
+    if(this.productForm.value.productNewStock){
+      const newStock=this.productForm.value.productNewStock;
+      this.productService.updateProductStockByCode(this.scannedBarcodeValue,newStock).subscribe(
+        product=>{
+          console.log(product);
+        }
+      )
+    }
+
+  }
+
+
 }
